357b7805835be2f4b8a8f1a595b6fc4e1fbae6f6
liuguiyangnwpu <liuguiyangnwpu@163.com>
2016-06-16 10:06:36 +0800

    add omp, can run but still not enough fast

--- source/algorithm/retrieve/ASIFT/extract_feature.h
+++ source/algorithm/retrieve/ASIFT/extract_feature.h
@@ -6,6 +6,16 @@
 #include <iostream>
 #include <vector>
 
+#ifdef _OPENMP
+
+#if defined(__clang__)
+#include <libiomp/omp.h>
+#elif defined(__GNUG__) || defined(__GNUC__)
+#include <omp.h>
+#endif
+
+#endif
+
 using namespace std;
 
 bool AsiftFeature(const string Output_FileName,string Input_FilePath, vector<vector<float>>& features);
